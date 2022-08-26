94f493d832b6d5b8fdee482b24bbd8612b39eb61
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-08-26 16:52:52 -0700

    add user controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,41 @@
+const { User, Player } = require('../models');
+
+
+const userCount = async () => 
+User.aggregate()
+.then((numberOfUsers) => numberOfUsers)
+
+
+module.exports = {
+    getUsers(req, res) {
+        User.find()
+        .then(async (users) => {
+            const userObj = {
+                users,
+                userCount: await userCount(),
+            };
+            return res.json('user:' + userObj);
+        })
+        .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+        });
+    },
+
+getSingleUser(req, res) {
+    User.findOne({ _id: req.params.UserId })
+      .select('-__v')
+      .lean()
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: 'No User with given id' })
+          : res.json({
+              user,
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+}
