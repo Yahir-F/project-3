47500a68d7777bd5106bb278622fc06253329856
Sean Oh <23ohsean@gmail.com>
2022-08-26 17:48:05 -0700

    basic page routes functionality

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,17 +1,23 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Header from './Header'
 import Footer from './Footer'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Game from './pages/Game'
 
 function Main() {
   return (
-    <div>
+    <Router>
         <Header/>
-        CURRENT PAGE
-        {/* set up page routes */}
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/game' element={<Game/>}/>
+        </Routes>
         <Footer/>
-    </div>
+    </Router>
   )
 }
 
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 function Navigation() {
   return (
-    <div>
-        {/* Set up nav links */}
-        Navigation
-    </div>
+    <ul>
+        <li><a href='/'>Home</a></li>
+        <li><a href='/login'>Login</a></li>
+        <li><a href='/game'>Game</a></li>
+    </ul>
   )
 }
 
