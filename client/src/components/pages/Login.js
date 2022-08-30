bfe2f800f9ee209f28fce3185b19e1551561c240
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-08-30 16:05:44 -0700

    login form

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className="log-form">
+    <h2>Login</h2>
+    <form>
+      <input
+      className='form-input'
+      placeholder= "username"
+      name='username'
+      // value={}
+      // onChange={}
+       type="text" 
+        />
+      <input 
+      className='form-input'
+      placeholder= "password"
+      type="password" 
+      // value={}  
+      // onChange={} 
+      />
+      <button 
+      type="submit"
+       className="btn">Login</button>
+    </form>
+  </div>
   )
 }
 
-export default Login
+export default Login;
