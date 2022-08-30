5bfe1ae6ac347062c319a1ae2509c765d9c2e46e
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-08-30 16:53:25 -0700

    new login

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-import Auth from '../../utils/auth';
+// import { Link } from 'react-router-dom';
+// import Auth from '../../utils/auth';
 
 function Login() {
   return (
+    <div className='doesntmatter'>
     <div className="log-form">
     <h2>Login</h2>
     <form>
@@ -27,7 +28,42 @@ function Login() {
        className="btn">Login</button>
     </form>
   </div>
+  
+
+
+    <div className="sign-form">
+    <h2>SignUp</h2>
+    <form>
+    <input 
+      className='form-input'
+      placeholder= "email"
+      type="email" 
+      // value={}  
+      // onChange={} 
+      />
+      <input
+      className='form-input'
+      placeholder= "username"
+      name='username'
+      // value={}
+      // onChange={}
+       type="text" 
+        />
+      <input 
+      className='form-input'
+      placeholder= "password"
+      type="password" 
+      // value={}  
+      // onChange={} 
+      />
+      <button 
+      type="submit"
+       className="btn">SignUp</button>
+    </form>
+  </div>
+  </div>
   )
 }
+
 
 export default Login;
