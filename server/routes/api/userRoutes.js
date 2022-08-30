702fa908af7890299ac2ced19fee0199daf98a5e
Sean Oh <23ohsean@gmail.com>
2022-08-30 16:27:21 -0700

    add new login/signup routes

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
+const { authMiddleware } = require('../../utils/auth')
+
 const {
   getUsers,
   getSingleUser,
+  createUser,
+  login,
   } = require('../../controllers/userController');
   
 
-  router.route('/').get(getUsers).post();
+  router.route('/').get(getUsers).post(createUser);
+
+  router.route('/login').post(login);
 
   router.route('/:userId').get(getSingleUser).delete();
 
