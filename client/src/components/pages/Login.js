bdcb9c23632d4afa91c8e8e019fd9c72dc97241e
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-08-30 17:44:14 -0700

    login work

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,33 +1,12 @@
 import React from 'react'
+import LoginForm from '../LoginForm'
 // import { Link } from 'react-router-dom';
 // import Auth from '../../utils/auth';
 
 function Login() {
   return (
     <div className='doesntmatter'>
-    <div className="log-form">
-    <h2>Login</h2>
-    <form>
-      <input
-      className='form-input'
-      placeholder= "username"
-      name='username'
-      // value={}
-      // onChange={}
-       type="text" 
-        />
-      <input 
-      className='form-input'
-      placeholder= "password"
-      type="password" 
-      // value={}  
-      // onChange={} 
-      />
-      <button 
-      type="submit"
-       className="btn">Login</button>
-    </form>
-  </div>
+   <LoginForm/>
   
 
 
