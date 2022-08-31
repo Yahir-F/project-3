bc9f98c4f9986656385d879521d6f453ce3b1e35
Sean Oh <23ohsean@gmail.com>
2022-08-31 15:35:03 -0700

    generate and display map

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import Auth from '../utils/auth'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import Auth from '../utils/auth';
+import { Link } from 'react-router-dom';
 
 
 function Navigation() {
@@ -10,27 +10,30 @@ function Navigation() {
   };
   return (
     <div>
-           <Link className="btn btn-lg btn-info m-2" to="/">
-          Home
-        </Link>
-    {Auth.loggedIn() ? (
-      <>
-        <Link className="btn btn-lg btn-info m-2" to="/me">
-          {Auth.getProfile().data.username}'s profile
-        </Link>
-        <button className="btn btn-lg btn-light m-2" onClick={logout}>
-          Logout
-        </button>
-      </>
-    ) : (
-      <>
-        <Link className="btn btn-lg btn-info m-2" to="/login">
-          Login/SignUp
-        </Link>
-      </>
-    )}
-  </div>
-  )
-    }
+      <Link className="btn btn-lg btn-info m-2" to="/">
+        Home
+      </Link>
+      <Link className="btn btn-lg btn-info m-2" to="/game">
+        Game
+      </Link>
+      {Auth.loggedIn() ? (
+        <>
+          <Link className="btn btn-lg btn-info m-2" to="/me">
+            {Auth.getProfile().data.username}'s profile
+          </Link>
+          <button className="btn btn-lg btn-light m-2" onClick={logout}>
+            Logout
+          </button>
+        </>
+      ) : (
+        <>
+          <Link className="btn btn-lg btn-info m-2" to="/login">
+            Login/SignUp
+          </Link>
+        </>
+      )}
+    </div>
+  );
+}
 
-export default Navigation
+export default Navigation;
