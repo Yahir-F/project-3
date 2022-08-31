9d8ca1b092bdbc6f99d181cfe064228e9552215c
Sean Oh <23ohsean@gmail.com>
2022-08-30 18:21:18 -0700

    add signup form functionality

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,68 +1,68 @@
 import React, { useState } from 'react';
-import Auth from '../utils/auth'
+import Auth from '../utils/auth';
 import { loginUser } from '../utils/api';
 
 const LoginForm = (props) => {
-    const [formState, setFormState] = useState({ username: '', password: '' });
+  const [formState, setFormState] = useState({ username: '', password: '' });
 
-  
-    // update state based on form input changes
-    const handleChange = (event) => {
-      const { name, value } = event.target;
-  
-      setFormState({
-        ...formState,
-        [name]: value,
-      });
-    };
-  
-    // submit form
-    const handleFormSubmit = async (event) => {
-      event.preventDefault();
-      console.log(formState);
-      try {
-        const response = await loginUser(formState)
-        const data = await response.json()
-        Auth.login(data.token);
-      } catch (e) {
-         console.log(e);
-      }
-  
-      // clear form values
-      setFormState({
-        username: '',
-        password: '',
-      });
-    };
-  return(
- 
 
-<div className="log-form">
-<h2>Login</h2>
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
 
-<form onSubmit={handleFormSubmit}>
-  <input
-  className='form-input'
-  placeholder= "username"
-  name='username'
-  value={formState.username}
-   onChange={handleChange}
-   type="text" 
-    />
-  <input 
-  name='password'className='form-input'
-  placeholder= "password"
-  type="password" 
-  value={formState.password}  
-  onChange={handleChange} 
-  />
-  <button 
-  type="submit"
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
 
-   className="btn">Login</button>
-</form>
-</div>
-)
-  }
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const response = await loginUser(formState);
+      const data = await response.json();
+      Auth.login(data.token);
+    } catch (e) {
+      console.log(e);
+    }
 
-  export default LoginForm;
+    // clear form values
+    setFormState({
+      username: '',
+      password: '',
+    });
+  };
+  return (
+
+
+    <div className="log-form">
+      <h2>Login</h2>
+
+      <form onSubmit={handleFormSubmit}>
+        <input
+          className='form-input'
+          placeholder="username"
+          name='username'
+          value={formState.username}
+          onChange={handleChange}
+          type="text"
+        />
+        <input
+          name='password' className='form-input'
+          placeholder="password"
+          type="password"
+          value={formState.password}
+          onChange={handleChange}
+        />
+        <button
+          type="submit"
+
+          className="btn">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,47 +1,16 @@
-import React from 'react'
-import LoginForm from '../LoginForm'
+import React from 'react';
+import LoginForm from '../LoginForm';
+import SignupForm from '../SignupForm';
 // import { Link } from 'react-router-dom';
 // import Auth from '../../utils/auth';
 
 function Login() {
   return (
     <div className='doesntmatter'>
-   <LoginForm/>
-  
-
-
-    <div className="sign-form">
-    <h2>SignUp</h2>
-    <form>
-    <input 
-      className='form-input'
-      placeholder= "email"
-      type="email" 
-      // value={}  
-      // onChange={} 
-      />
-      <input
-      className='form-input'
-      placeholder= "username"
-      name='username'
-      // value={}
-      // onChange={}
-       type="text" 
-        />
-      <input 
-      className='form-input'
-      placeholder= "password"
-      type="password" 
-      // value={}  
-      // onChange={} 
-      />
-      <button 
-      type="submit"
-       className="btn">SignUp</button>
-    </form>
-  </div>
-  </div>
-  )
+      <LoginForm />
+      <SignupForm />
+    </div>
+  );
 }
 
 
