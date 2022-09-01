4bd3c65bbb2f1b6dd6d13d002bf63f6d5d540af1
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-09-01 16:39:58 -0700

    fix deployment

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
-
+const path = require('path');
 
 
 const PORT = process.env.PORT || 3001;
@@ -10,16 +10,16 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
+app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API running on port ${PORT}!`);
