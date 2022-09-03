13b21fe2a187a7bf77eebf572aa08f2f76adc8c3
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-09-03 14:09:33 -0700

    remove unused import

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -3,7 +3,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import { Typography } from "@mui/material";
-import brick from "../../images/main.jpg"
+
 
 
 
