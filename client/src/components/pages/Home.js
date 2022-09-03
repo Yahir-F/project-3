cfeb62ab63313316570b322e0a2100e2364331d6
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-09-03 14:02:27 -0700

    fill blank image & instructions

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,10 +1,37 @@
-import { Box } from '@mui/material'
 import React from 'react'
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import { Typography } from "@mui/material";
+import brick from "../../images/main.jpg"
+
+
 
 function Home() {
   return (
-    <Box sx={{ flexGrow: 1, position: 'fixed', bottom: 0, left: 0, right: 0 }}>WIP</Box>
-  )
+    <React.Fragment>
+    <CssBaseline />
+    <Box sx={{color: "white", padding:'30px', }}>
+    <Container maxWidth="sm" sx={{ margin: "20px auto", paddingTop: "50px" }}>
+      <Box sx={{color: "white", padding:'30px', backgroundColor:'black' }}>
+        <Typography sx={{ textAlign: "center" }}>
+          WELCOME TO ROGUESCAPE
+          <br />
+          INTRUCTIONS: <br />
+          Movement: Arrow Keys Up,Down,Left,Right <br />
+          Move Against Enemy to engage in Combat Fight enemies and Level Up{" "}
+          <br />
+          Only the Strongest Make it to the End!!! <br />
+          This game was created for players with interests in Rougelike Games.{" "}
+          <br sx={{ backgroundColor: "yellow" }} />
+          CREATE AN ACCOUNT FOR THE BEST USER EXPERIENCE
+        </Typography>
+      </Box>
+    </Container>
+    </Box>
+  </React.Fragment>
+);
 }
 
-export default Home
+
+export default Home;
